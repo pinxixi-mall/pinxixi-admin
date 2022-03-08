6416795ad8defc3320f2f142757dacd9534c8d34
ye_tianmao <707764205@qq.com>
2022-03-08 23:32:43 +0800

    update

--- src/views/userManage/userInfo/index.tsx
+++ src/views/userManage/userInfo/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import { Card, Avatar } from 'antd';
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+
+const { Meta } = Card;
 
 const UserInfo: React.FC = () => {
   return (
-    <div>
-      user info
-    </div>
+    <>
+      <section>
+        <Card
+          style={{ width: 400 }}
+          cover={
+            <img
+              alt="example"
+              src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+            />
+          }
+          actions={[
+            <SettingOutlined key="setting" />,
+            <EditOutlined key="edit" />,
+            <EllipsisOutlined key="ellipsis" />,
+          ]}
+        >
+          <Meta
+            avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+            title="Card title"
+            description="This is the description"
+          />
+        </Card>,
+      </section>
+    </>
   )
 }
 
